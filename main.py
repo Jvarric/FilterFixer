24fa9fa4f2daf64f5fbccb42a02b815931c865b5
zvx <phersick@gmail.com>
2016-04-05 23:29:27 -0400

    First commit, general outline of program and working regex matching and output

---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+
+#REGEX = re.compile(r'(?:(.+?),(.*),(.+))')
+
+
+def main():
+    with open('filters.txt') as my_file:
+        filters = my_file.readlines()
+
+    for line in filters:
+        print(line, end="")
+        line = line.rstrip()
+        match = re.match(r'(?:(.+?),(.*),(.+))', line)
+
+        if match:
+            print("The matches are: {}".format(match.groups()))
+            print("The re-ordered filter is: {}\n".format(','.join(match.group(1, 3, 2))))
+
+#if __name__ == "__main()__":
+main()
